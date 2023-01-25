8073b6efb162ae2549800df197b26c670e1dbaff
jtschoonhoven <jtschoonhoven@gmail.com>
2023-01-24 23:32:46 -0800

    Demo normalized schema with foreign keys

--- app/generate_sql.py
+++ app/generate_sql.py
@@ -3,94 +3,137 @@ https://github.com/jerryjliu/gpt_index/blob/main/examples/struct_indices/SQLInde
 https://github.com/jerryjliu/gpt_index/blob/main/examples/struct_indices/SQLIndexDemo-Context.ipynb
 https://github.com/jerryjliu/gpt_index/issues/204
 """
+from dataclasses import dataclass
+from typing import Dict, List, Optional
 import gpt_index as gpt
 from gpt_index.indices.query.query_runner import QueryRunner
 from gpt_index.indices.query.struct_store.sql import GPTNLStructStoreIndexQuery
+from gpt_index.prompts.default_prompts import DEFAULT_TEXT_TO_SQL_PROMPT
 import sqlalchemy as db
 from gpt_index.indices.query.query_map import get_query_cls
 import langchain
 from app.env import OPENAI_API_KEY
 
+# Configure SqlAlchemy
 engine = db.create_engine("sqlite:///:memory:")
 metadata_obj = db.MetaData(bind=engine)
 
+# Configure LLM
 llm = langchain.OpenAI(temperature=0, model_name="text-davinci-002", openai_api_key=OPENAI_API_KEY)
 llm_predictor = gpt.LLMPredictor(llm=llm)
+prompt_helper = gpt.PromptHelper.from_llm_predictor(llm_predictor)
 
-table_name = "city_stats"
-city_stats_table = db.Table(
-    table_name,
-    metadata_obj,
-    db.Column("city_name", db.String(16), primary_key=True),
-    db.Column("population", db.Integer),
-    db.Column("country", db.String(16), nullable=False),
-)
+
+@dataclass
+class TableSchemaCol:
+    name: str
+    type: db.types.TypeEngine
+    nullable: bool = False
+    primary_key: bool = False
+    fk: Optional[str] = None
+
+
+@dataclass
+class TableSchema:
+    name: str
+    desc: str
+    cols: List[TableSchemaCol]
+
+
+# Define all tables
+TABLES: List[TableSchema] = [
+    TableSchema(
+        name='users',
+        desc=(
+            'This table gives information regarding each user (AKA person, customer) in the app. '
+            'In this table, the column "created_at" refers to when a user joined.'
+        ),
+        cols=[
+            TableSchemaCol('id', db.Integer, primary_key=True),
+            TableSchemaCol('full_name', db.String(256)),
+            TableSchemaCol('created_at', db.DateTime),
+        ],
+    ),
+    TableSchema(
+        name='user_logins',
+        desc=(
+            'This table gives information regarding the datetime (AKA timestamp, AKA date) of each user login event. '
+            'In this table, the column "created_at" refers to the the date and time of a given login.'
+        ),
+        cols=[
+            TableSchemaCol('id', db.Integer, primary_key=True),
+            TableSchemaCol('user_id', db.ForeignKey('users.id')),
+            TableSchemaCol('created_at', db.DateTime),
+        ],
+    ),
+    TableSchema(
+        name='products',
+        desc=(
+            'This table gives information regarding each product (AKA item, subscription) for sale in the app. '
+            'In this table, the column "amount_cents" refers to the price of an item in cents (USD).'
+        ),
+        cols=[
+            TableSchemaCol('id', db.Integer, primary_key=True),
+            TableSchemaCol('amount_cents', db.Integer),
+            TableSchemaCol('created_at', db.DateTime),
+        ],
+    ),
+    TableSchema(
+        name='users_purchases',
+        desc=(
+            'This table gives information regarding each purchase (AKA sale) for each user and product. '
+            'Total revenue may be calculated by joining the "products" table and summing "products.amount_cents".'
+        ),
+        cols=[
+            TableSchemaCol('id', db.Integer, primary_key=True),
+            TableSchemaCol('user_id', db.ForeignKey('users.id')),
+            TableSchemaCol('product_id', db.ForeignKey('products.id')),
+            TableSchemaCol('created_at', db.DateTime),
+        ],
+    ),
+]
+
+# Create tables
+for table in TABLES:
+    db.Table(
+        table.name,
+        metadata_obj,
+        *[db.Column(col.name, col.type, nullable=col.nullable, primary_key=col.primary_key) for col in table.cols],
+    )
+
+# Create DB
 metadata_obj.create_all()
+sql_database = gpt.SQLDatabase(engine, include_tables=[table.name for table in TABLES])
 
-sql_database = gpt.SQLDatabase(engine, include_tables=["city_stats"])
-sql_database.table_info
 
-city_stats_text = (
-    "This table gives information regarding the population and country of a given city.\n"
-    "The user will query with codewords, where 'foo' corresponds to population and 'bar'"
-    "corresponds to city."
-)
+def _get_all_tables_desc() -> str:
+    """Get tables schema + optional context as a single string."""
+    tables_desc = []
+    for table_name in sql_database.get_table_names():
+        table_desc = sql_database.get_single_table_info(table_name)
+        table_text = f"Schema of table {table_name}:\n" f"{table_desc}\n"
+        table = next((table for table in TABLES if table.name == table_name), None)
+        if table:
+            table_text += f"Context of table {table.name}:\n"
+            table_text += table.desc
+        tables_desc.append(table_text)
+    result = "\n\n".join(tables_desc)
+    print(result)
+    return result
 
-# manual
-table_context_dict = {"city_stats": city_stats_text}
-index = gpt.GPTSQLStructStoreIndex(
-    documents=[],
-    sql_database=sql_database,
-    table_name="city_stats",
-    table_context_dict=table_context_dict,
-    llm_predictor=llm_predictor,
-)
 
-# BaseGptIndexQuery.query
-query_config = gpt.QueryConfig(
-    index_struct_type=gpt.IndexStructType.from_index_struct(index.index_struct),
-    query_mode=gpt.QueryMode.DEFAULT,
-)
-query_runner = QueryRunner(
-    index._llm_predictor,
-    index._prompt_helper,
-    index._embed_model,
-    index._docstore,
-    query_configs=[query_config],
-    verbose=True,
-    recursive=False,
-)
-
-# # QueryRunner.query
-index_struct_type = gpt.IndexStructType.from_index_struct(index.index_struct)
-if index_struct_type not in query_runner._config_dict:
-    raise ValueError(f"IndexStructType {index_struct_type} not in config_dict")
-config = query_runner._config_dict[index_struct_type]
-mode = config.query_mode
-query_cls = get_query_cls(index_struct_type, mode)
-query_kwargs = query_runner._get_query_kwargs(config)
-query_obj: GPTNLStructStoreIndexQuery = query_cls(
-    index.index_struct,
-    query_runner=query_runner,
-    docstore=query_runner._docstore,
-    sql_database=sql_database,
-    **query_kwargs,
-)
-
-# GPTNLStructStoreIndexQuery._query
-table_desc_str = query_obj._get_all_tables_desc()
-
+tables_schema = _get_all_tables_desc()
 
 if __name__ == '__main__':
     while True:
         query_str = input('\nEnter a database query in plain english, or enter "q" to exit\n')
         if query_str == 'q':
             break
-        response_str, _ = query_obj._llm_predictor.predict(
-            query_obj._text_to_sql_prompt,
-            query_str=query_str,
-            schema=table_desc_str,
-        )
-        sql_query_str = query_obj._parse_response_to_sql(response_str)
+        sql_query_str, _ = llm_predictor.predict(DEFAULT_TEXT_TO_SQL_PROMPT, query_str=query_str, schema=tables_schema)
         print('\n')
         print(sql_query_str)
+        try:
+            sql_database.run_sql(sql_query_str)
+            print('VALID')
+        except Exception:
+            print('NOT VALID')
